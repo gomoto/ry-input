b209119627fbfaa6f419d5a4818aa916b116f4c3
Ryan Gomoto <ryan.gomoto@gmail.com>
2015-10-30 01:18:36 -0700

    Update input width once after CSS loads, in case first update happened before CSS loaded

--- ryInput.directive.js
+++ ryInput.directive.js
@@ -21,39 +21,50 @@ angular.module('ryInputDemo')
       //Insert element guide after element
       //but it doesn't matter where guide is (position: fixed)
       element.after(elementGuide);
+
+      function updateElementWidth(modelValue) {
+        //input text --> guide text
+        elementGuide.html(modelValue);
+        //input text style --> guide text style
+        var elementComputedStyle = $window.getComputedStyle(element[0]);
+        angular.forEach([
+          'border-left-style',
+          'border-left-width',
+          'border-right-style',
+          'border-right-width',
+          'font-family',
+          'font-kerning',
+          'font-size',
+          'font-stretch',
+          'font-style',
+          'font-variant',
+          'font-variant-ligatures',
+          'font-weight',
+          'letter-spacing',
+          'max-width',
+          'min-width',
+          'padding-left',
+          'padding-right',
+          'text-indent',
+          'text-transform',
+          'word-spacing'
+        ], function(property) {
+          elementGuide.css(property, elementComputedStyle[property]);
+        });
+        //guide width --> input width
+        var elementGuideWidth = $window.getComputedStyle(elementGuide[0]).width;
+        element.css('width', elementGuideWidth);
+      }
+
       if (attributes.ngModel) {
-        scope.$watch(attributes.ngModel, function(modelValue) {
-          //input text --> guide text
-          elementGuide.html(modelValue);
-          //input text style --> guide text style
-          var elementComputedStyle = $window.getComputedStyle(element[0]);
-          angular.forEach([
-            'border-left-style',
-            'border-left-width',
-            'border-right-style',
-            'border-right-width',
-            'font-family',
-            'font-kerning',
-            'font-size',
-            'font-stretch',
-            'font-style',
-            'font-variant',
-            'font-variant-ligatures',
-            'font-weight',
-            'letter-spacing',
-            'max-width',
-            'min-width',
-            'padding-left',
-            'padding-right',
-            'text-indent',
-            'text-transform',
-            'word-spacing'
-          ], function(property) {
-            elementGuide.css(property, elementComputedStyle[property]);
-          });
-          //guide width --> input width
-          var elementGuideWidth = $window.getComputedStyle(elementGuide[0]).width;
-          element.css('width', elementGuideWidth);
+        scope.$watch(attributes.ngModel, updateElementWidth);
+        //Angular runs right after DOMContentLoaded event, so this link function
+        //could run before CSS is applied.
+        //In that situation, the first updateElementWidth() gets incorrect computed styles.
+        //To fix this, run updateElementWidth() one time right after CSS loads.
+        $window.addEventListener('load', function onWindowLoad() {
+          $window.removeEventListener('load', onWindowLoad);
+          updateElementWidth();
         });
       }
     }
